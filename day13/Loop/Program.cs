e39c3d71545678a23a0b681be2222dc1c79f32f7
adithyaark <adithya6119@gmail.com>
2026-02-19 21:31:01 +0530

    added Loop files

---
+++ day13/Loop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      Random random = new Random();
+      int target = random.Next(1, 101);
+      int guess = 0;
+
+      while(guess != target)
+      {
+        Console.Write("enter your guess:");
+        guess = Convert.ToInt32(Console.ReadLine());
+
+        if (guess < target)
+        {
+          Console.WriteLine("Too low!");
+        }
+        else if (guess > target )
+        {
+          Console.WriteLine("Too High!!");
+        }
+        else 
+        {
+          Console.WriteLine("GREAT! You guessed it!");
+        }
+      }
+      
+        
+    }
+}
