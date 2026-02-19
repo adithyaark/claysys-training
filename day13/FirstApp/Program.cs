b8dccbd4fe840111dd7ba3756f0080d7885df50f
adithyaark <adithya6119@gmail.com>
2026-02-19 19:16:49 +0530

    Added day 13 files

---
+++ day13/FirstApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Security;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 12;
+        int b = 14;
+
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+        double div = a / b;
+
+        Console.WriteLine(a + "+" + b + "=" + sum);
+        Console.WriteLine(a + "-" + b + "=" + diff);
+        Console.WriteLine(a + "*" + b + "=" + prod);
+        Console.WriteLine(a + "/" + b + "=" + div);
+
+        int age = 40;
+        string x = "adult";
+        string y = "minor";
+
+        if ( age >=18)
+        {
+          Console.Write(x+ " since age is " + age);
+        }
+        else
+        {
+          Console.Write(y+ " since age is " + age);
+        }
+    }
+}
